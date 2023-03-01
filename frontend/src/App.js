2e362e0a508ed26c12eae99160401386cabce9f3
Jennifer Lin <jenniferlin124@gmai.com>
2023-02-28 22:46:11 -0800

    created Sign up and Sign in page + linked only for localhost

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 function App() {
   return (
     <div className="App">
-      
+    <Router>
+      <Routes>
+        {/* <Route exact path="/" element={<Home />} /> */}
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
 
 export default App;
+
---
+++ frontend/src/pages/SignIn/SignIn.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Checkbox, CssBaseline, FormControlLabel, TextField } from '@mui/material';
+
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (mainColor) => augmentColor({ color: { main: mainColor } });
+const themeLight = createTheme({
+    
+    palette: {
+        retyrn_blue: createColor('#4ca7da'),
+        background: {
+            default: "#fafafa"
+        }
+    }
+});
+
+export default function SignIn() {
+    return(
+        <ThemeProvider theme={themeLight}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <Box sx={{ marginTop:20, display:'flex', flexDirection:'column', alignItems:'center'}}>
+                    <Typography component="h1"variant="h4">
+                        Sign In
+                    </Typography>
+                    <Box component="form">
+                        <TextField required fullWidth id="email" label="Email Address" name="email" margin="normal" autoComplete="email" autoFocus/>
+                        <TextField required fullWidth id="password" label="Password" name="password" type="password" margin="normal" autoComplete="current-password"/>
+                        <FormControlLabel control={<Checkbox value="remember" color="retyrn_blue"/>} label="Remember me"/>
+                        <Button type="submit" fullWidth variant="contained" sx={{mt:3, mb:2, color: 'white', ':hover':{ bgcolor: '#6fb8e1'}}} color="retyrn_blue">Sign In</Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="http://localhost:3000/signup#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+            </Box>
+        </Container>
+        </ThemeProvider>
+    );
+}
