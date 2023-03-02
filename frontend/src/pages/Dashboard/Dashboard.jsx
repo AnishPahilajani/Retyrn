fc6122594a1a14464823961e0b1f4d51c78b48da
Obsidian <obsidian@Obsidians-MacBook-Pro.local>
2023-03-01 19:43:46 -0800

    Changed background

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -13,7 +13,21 @@ import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import { Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (mainColor) => augmentColor({ color: { main: mainColor } });
+
+const themeLight = createTheme({
+    
+    palette: {
+        retyrn_blue: createColor('#4ca7da'),
+        background: {
+            default: "#F2F2F2"
+        }
+    }
+});
 const drawerWidth = 240;
 
 export default function Dashboard(props) {
@@ -59,72 +73,76 @@ export default function Dashboard(props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex', }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >  
-        <Toolbar>
-            {/* This is the menu icon. Once it is clicked, it will 
-            toggle handleDrawerToggle to which will open the drawer */}
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* When on mobile view, the drawer is closed unless the user clicks on the hamburger icon */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
+    <ThemeProvider theme={themeLight}>
+        <Box sx={{ display: 'flex', }}>
+        <CssBaseline />
+        <AppBar
+            position="fixed"
+            sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+            backgroundColor:'#FFFFFF',
+            color: '#080D1C'
+            }}
+        >  
+            <Toolbar>
+                {/* This is the menu icon. Once it is clicked, it will 
+                toggle handleDrawerToggle to which will open the drawer */}
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+                <MenuIcon />
+            </IconButton>
+            </Toolbar>
+        </AppBar>
+        <Box
+            component="nav"
+            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+            aria-label="mailbox folders"
         >
-          {drawer}
-        </Drawer>
-        {/* When on Desktop view, the drawer is always shown. */}
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
+            {/* When on mobile view, the drawer is closed unless the user clicks on the hamburger icon */}
+            <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+                display: { xs: 'block', sm: 'none' },
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+            >
+            {drawer}
+            </Drawer>
+            {/* When on Desktop view, the drawer is always shown. */}
+            <Drawer
+            variant="permanent"
+            sx={{
+                display: { xs: 'none', sm: 'block' },
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+            open
+            >
+            {drawer}
+            </Drawer>
+        </Box>
 
-      {/* Main component to which displays whatever we want. */}
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` }}}
-      >
-        <Toolbar />
-        {/* Under this toolbar is where want to display things */}
-        <Typography>For example: Hi</Typography>
-      </Box>
-    </Box>
+        {/* Main component to which displays whatever we want. */}
+        <Box
+            component="main"
+            sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` }}}
+        >
+            <Toolbar />
+            {/* Under this toolbar is where want to display things */}
+            <Typography>For example: Hi</Typography>
+        </Box>
+        </Box>
+    </ThemeProvider>
   );
 }
